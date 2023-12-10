71d4683f78880d9db933124098c8932c18a106fa
slm1906 <slm1906@g.rit.edu>
2023-12-10 16:16:26 -0500

    add stuff to drink page

--- src/app/drink/page.tsx
+++ src/app/drink/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { DRINKS } from "@/components/constants";
 import { Ingredient, Drink } from "@/components/types";
+import Link from "next/link";
 
 
 
@@ -43,22 +44,25 @@ export default function DrinkPage(props: { params: {}, searchParams: { drinkName
     })
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            
+        <main className="flex min-h-screen flex-col justify-between p-12">
+            <div className='dark:hidden p-5 text-left'><Link href={"/"}><Image src={"/light-logo.png"} width={160} height={80} alt="" /></Link></div>
+
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <div>
-                    <Image 
+                    <Image className="self-center rounded-full md:justify-self-start"
                         src={drink.image} // Route of the image file
                         height={400} // Desired size with correct aspect ratio
                         width={400} // Desired size with correct aspect ratio
                         alt={drink.image_alt === undefined? "": drink.image_alt}
                     />
+                    <p className="text-2xl align-center pt-10 pb-5">{drink.name}</p>
+                    <p className="align-center pb-5">{drink.description_long}</p>
                     
                     <IngredientList ingredients={drink.ingredients_print}/>
                     <br/>
                     <DirectionsList directions={drink.instructions}/>
                     <br/>
-                    <a href={drink.source}>Source</a>
+                    <a href={drink.source} className="underline bold underline-offset-2">Source</a>
                 </div>
             </div>
     </main>
